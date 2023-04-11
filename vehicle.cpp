2485e5f72865437924e81fc2f72d10440cc91f84
jiangzizi <124546648+jiangzizi@users.noreply.github.com>
2023-04-11 16:34:05 +0800

    Delete vehicle.cpp

--- vehicle.cpp
+++
@@ -1,6 +0,0 @@
-#include "vehicle.h"
-void Vehicle::describe()
-{
-    cout << "Finish building a vehicle with " << wnum << " wheels and a " << ename << " engine." << endl
-         << "A vehicle with "<<wnum<< " wheels and a "<<ename<<" engine."<<endl;
-}
