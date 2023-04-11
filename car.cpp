669ee2cc9ddd708fc22f694024d9869d184a84fc
jiangzizi <124546648+jiangzizi@users.noreply.github.com>
2023-04-11 16:32:53 +0800

    Delete car.cpp

--- car.cpp
+++
@@ -1,6 +0,0 @@
-#include "car.h"
-void Car::describe()
-{
-    cout << "Finish building a car with " << mnum << " wheels and a " << mname << " engine." << endl
-         << "A car with " << mnum << " wheels and a " << mname << " engine." << endl;
-}
