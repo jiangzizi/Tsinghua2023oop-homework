320765955541afc6e1eb93f84e4a542bf984c214
jiangzizi <124546648+jiangzizi@users.noreply.github.com>
2023-05-22 15:50:25 +0800

    Add files via upload

---
+++ Lab5/t3/editor.cpp
@@ -0,0 +1,10 @@
+#include"editor.h"
+TextEditor:: TextEditor(){}
+    void TextEditor:: moveCursor(Position dest){}
+    void TextEditor:: select(Position beg, Position end){}
+    void TextEditor:: write(const std::string& str){}
+    void TextEditor:: newline(){}
+    void TextEditor:: del(){}
+    void TextEditor:: copy(){}
+    void TextEditor:: paste(){}
+    void TextEditor:: screenShot(){}
